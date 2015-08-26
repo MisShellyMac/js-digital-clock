4c66db741c0f9a51c18a15f5b10284375969e5af
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-25 22:50:55 -0700

    My Digital Clock Solution

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function displayTime(){
+  var currentTime = new Date();
+  console.log(currentTime);
+  // alert(currentTime);
+
+  var clockDiv = document.getElementById("clock");
+  console.log(clockDiv);
+
+  function regularTime(hours){
+    if (hours < 12) {return hours;}
+    else if (hours > 12) {return hours - 12;}
+  }
+
+  function addZero(unit) {
+  unit = unit < 10 ? "0"+ unit : unit;
+  return unit;
+  }
+
+  var meridian = (Hours < 12) ? "AM" : "PM";
+  var Hours = addZero(regularTime(currentTime.getHours()));
+  var Minutes = addZero(currentTime.getMinutes());
+  var Seconds = addZero(currentTime.getSeconds());
+  var Year = currentTime.getFullYear();
+
+  clockDiv.innerText = Hours + ":" + Minutes + ":" + Seconds + " " + meridian + "\n" +Year;
+
+}
+
+displayTime();
+setInterval(displayTime, 1000)
